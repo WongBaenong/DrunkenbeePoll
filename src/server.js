27279bb755a8badb236788d1996b7d392681ee81
WongBaenong <anmsu94@naver.com>
2022-08-09 18:21:54 +0900

    Create poll system

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", rootRouter);
+
+export default app;
